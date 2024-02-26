1cc0a86609640b0f6acb0765775cbea2402324b1
DesG01211517 <desmongarrison2@gmail.com>
2024-02-26 17:33:35 -0500

    almost

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-guessButton.onclick = () {
-    let bald = baldCheckBox.checked;
-    let glasses = glassesCheckBox.checked;
-    let hat = hatCheckBox.checked;
-    console.log(baldCheckBox)
+guessButton.onclick = function () {
+    let isBald = baldCheckbox.checked;
+    let isGlasses = glassesCheckbox.checked;
+    let isHat = hatCheckbox.checked;
+    
+
+
+    let isSam = isBald && isGlasses  && !isHat;
+    let isMike = !isBald && !isGlasses && !isHat;
+    let isEric =  !isBald && !isGlasses && isHat;
+
+
+    if (isSam) {
+        alert = (`It's Me Sam!`);
+        samImg.src = ("Img/sam.jpg");
+        divA.style.backgroundColor = `green`;
 }
-
-
-
-
-
-let sam = bald && glasses  && !hat;
-let mike = !bald && !glasses && !hat;
-let eric =  !bald && !glasses && hat;
-
-
-if (sam) {
-    divAtextContent = `It's Me Sam!`;
-    samImg.src = `sam.jpg`;
-    divB.style.backgroundColor = `green`
+    else if (isMike) {
+        alert = (`It's Me Mike!`);
+        mikeImg.src = ("Img/mike.jpg");
+        divB.style.backgroundColor = `green`;
 }
-else if (mike) {
-    divBtextContent = `It's Me Mike!`;
-    mikeImg.src = `mike.jpg`;
-    divB.style.backgroundColor = `green`
+    else if (isEric) {
+        alert = (`It's Me Eric`);
+        ericImg.src = ("Img/eric.jpg");
+        divC.style.backgroundColor = `green`;
+
 }
-else if (eric) {
-    divCtextContent = `It's Me Mike!`;
-    mikeImg.src = `mike.jpg`;
-    divC.style.backgroundColor = `green`
 }
